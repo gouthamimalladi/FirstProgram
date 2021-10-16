acccf6d490d476c9ce931c4f6caff66f73feefe0
gouthamimalladi <90868143+gouthamimalladi@users.noreply.github.com>
2021-10-16 00:15:56 -0700

    Update HelloWorld.java

--- test/sample/HelloWorld.java
+++ test/sample/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("My First Program");
+        System.out.println("First github program");
     }
 }
