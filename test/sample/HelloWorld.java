9f3e7cf9a1275ce46cb9a82a1145bb4512b5b326
gouthamimalladi <gouthami.reddy48@gmail.com>
2021-10-16 00:12:23 -0700

    Added new functionality

--- test/sample/HelloWorld.java
+++ test/sample/HelloWorld.java
@@ -2,5 +2,6 @@ package test.sample;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("My First Program");
     }
 }
