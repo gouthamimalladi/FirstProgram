8a0f93763f9337b76e97a9c060f766d71a5389fe
gouthamimalladi <gouthami.reddy48@gmail.com>
2021-10-18 12:47:50 -0700

    Added new functionality

---
+++ Loop_Strings.java
@@ -0,0 +1,12 @@
+public class Loop_Strings {
+    public static void main(String[] args) {
+
+        String s[] = {"gouthami","rani","rithvik","rohan"};
+        System.out.println( " array count" +s.length);
+        for(int i =0;i<=s.length;i++){
+
+        }
+        System.out.println(s[3]);
+
+    }
+}
